b0f49b1ec1ffb9a1dd7fab3807d679d7e97661ca
H2212 <viktorholyak1@gmail.com>
2026-03-08 12:35:06 +0200

    changed a lot

--- first.py
+++ first.py
@@ -1,8 +1,13 @@
 class Student:
-    def __init__(self, height = 160):
+    amount_of_students = 0
+
+    def __init__(self, height=160):
         self.height = height
         self.width = 151
+        Student.amount_of_students += 1
 
 jon = Student()
 kim = Student(height=149)
-print(f"I am {jon.height} kilometers tall and {jon.width} miles wide. And to remind you, yes, im taller than kim, hes only {kim.height} kilometers tall")
+
+print(f"I am {jon.height} kilometers tall and {jon.width} miles wide. And to remind you, yes, I'm taller than Kim, he's only {kim.height} kilometers tall")
+print(f"Jon has {jon.amount_of_students} and Kim has {kim.amount_of_students}")
